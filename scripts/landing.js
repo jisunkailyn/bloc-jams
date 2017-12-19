57b0b6ed17c2749d15601ac129e235712cf08777
Kailyn Lim <jisunkailyn@gmail.com>
2017-12-19 17:34:45 -0600

    Assignment 22 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+  points.style.opacity = 1;
+  points.style.transform = "scaleX(1) translateY(0)";
+  points.style.msTransform = "scaleX(1) translateY(0)";
+  points.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-  for (var i = 0; i < points.length; i++) {
-    revealPoint(i);
-  }
+  forEach(points, revealPoint);
 };
 
 window.onload = function() {
@@ -17,7 +16,7 @@ window.onload = function() {
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  
+
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
