d7ef395598bee3a21fc7e0c9a3c084b086543640
Kailyn Lim <jisunkailyn@gmail.com>
2017-12-21 00:24:12 -0600

    Assignment 22 DOM Scripting Events Complete 2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,25 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-  points.style.opacity = 1;
-  points.style.transform = "scaleX(1) translateY(0)";
-  points.style.msTransform = "scaleX(1) translateY(0)";
-  points.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
+var revealPoint = function(element) {
+  element.style.opacity = 1;
+  element.style.transform = "scaleX(1) translateY(0)";
+  element.style.msTransform = "scaleX(1) translateY(0)";
+  element.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
 
 var animatePoints = function(points) {
   forEach(points, revealPoint);
 };
 
 window.onload = function() {
-  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  //Automatically animate the points on a tall screen where scrolling can't trigger the animation
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-  window.addEventListener("scroll", function(event) {
+  window.addEventListener('scroll', function(event) {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
       animatePoints(pointsArray);
     }
