c7d10b6aca70d51eb9d7e83fc204fdf43a06cafa
Kailyn Lim <jisunkailyn@gmail.com>
2018-01-07 15:57:55 -0600

    Assignment 26 DOM Scripting Play/Pause Part 2 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -68,8 +68,16 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+        if (!currentParent) {
+          console.log("No parent found");
+          return;
+        }
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+            if (!currentParent) {
+              console.log("No parent found with that class name");
+              break;
+            }
         }
         return currentParent;
     }
